ab1433c65b042194b33d40c3c25af4887295dd62
23030175-wq <23030175@student.bdu.edu.vn>
2026-07-16 08:17:24 +0700

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import pandas as pd
 
 st.set_page_config(page_title="Order Nhà Hàng", layout="wide")
 
-st.title("🍽️ Hệ thống Order Nhà Hàng MR. BÌNH - Giảm 5% cho hoá đơn trên 1 triệu")
+st.title("🍽️ Hệ thống Order Nhà Hàng MR. TUYỀN NHÓM CHỦ ĐỀ 2 - Giảm 50% cho hoá đơn trên 1 triệu")
 
 # Thực đơn
 menu = {
