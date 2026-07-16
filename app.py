85820b42e198a0d26c2cf04c6592964612f85962
23030175-wq <23030175@student.bdu.edu.vn>
2026-07-16 08:28:49 +0700

    Update app.py

--- app.py
+++ app.py
@@ -57,7 +57,7 @@ with col2:
         st.table(df[["Tên món", "Đơn giá", "Số lượng", "Thành tiền"]])
         
         tam_tinh = df["Thành tiền"].sum()
-        giam_gia = (tam_tinh * 5) if tam_tinh > 1000000 else 0
+        giam_gia = (tam_tinh * 0.5) if tam_tinh > 1000000 else 0
         
         if giam_gia > 0:
             st.info(f"🎉 Giảm 50% cho hóa đơn trên 1 triệu!")
